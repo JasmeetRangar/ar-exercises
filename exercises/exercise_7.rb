588ac7d298a9cad268afd1e04757f7d3f6cf500d
Jasmeet Rangar <jasmeetsingr@gmail.com>
2021-03-30 17:03:18 +0000

    Add exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,10 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Enter a store name: "
+store_name = gets.chomp
+store7 = Store.create(name: store_name)
+store7.errors.messages.each do | key, value |
+  puts "#{key} error: #{value}"
+end
